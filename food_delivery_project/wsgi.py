6604581a8c1c2e77e24ffc3a66831cece1b29dcd
hakameida <hakameida@gmail.com>
2026-01-15 13:42:35 +0300

    Configure for redner deployment

--- food_delivery_project/wsgi.py
+++ food_delivery_project/wsgi.py
@@ -2,15 +2,16 @@
 WSGI config for food_delivery_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_delivery_project.settings')
 
 application = get_wsgi_application()
+
+# Apply WhiteNoise middleware for static files
+application = WhiteNoise(application)
